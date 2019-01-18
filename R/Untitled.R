c9de9690bf15ddc7e1777148bdf94514a9e9ca4b
Andrew Wilson <andrew.wilson849@icloud.com>
2019-01-18 12:03:30 -0800

    added first R script

---
+++ R/Untitled.R
@@ -0,0 +1,15 @@
+#' My function
+#'
+#' this function does something
+#'
+#' @param x is a numeric vector
+#' @return x squared
+#'
+#' @examples
+#' test_sqr(5)
+#'
+#' @export
+#'
+test_sqr <- function(x){
+  x^2
+}
