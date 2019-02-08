a98f34db4ea7573eb10bde3bc76d1b2c6f8bf336
Andrew Wilson <andrew.wilson849@icloud.com>
2019-02-08 14:51:08 -0800

    added example-tests.r file, removed the read-matrix tests for now

---
+++ tests/testthat/test-examples.R
@@ -0,0 +1,5 @@
+context("Example data")
+
+test_that("ggParanoid_example returns the correct number of files", {
+  testthat::expect_equal(length(ggParanoid::ggParanoid_example()), 1)
+})
--- tests/testthat/test-read_homolog_matrix.R
+++ tests/testthat/test-read_homolog_matrix.R
@@ -1,26 +1,5 @@
-context("ggParanoid")
+context("Example data")
 
-test_that("example data file exists", {
-  dir <- ggParanoid::ggParanoid_example_dir()
-  filename <- "/homology_matrix.csv.gz"
-  testthat::expect_true(file.exists(paste0(dir, filename)))
-})
-
-test_that("read_matrix() successfully imports data.table object", {
-  dir <- ggParanoid_example_dir()
-  filename <- "/homology_matrix.csv.gz"
-  h_mat <- ggParanoid:::read_matrix(paste0(dir, filename))
-  testthat::expect_true(all(c("data.table", "data.frame") %in% class(h_mat)))
-})
-
-test_that("to_matrix() properly converts data.table object to matrix object", {
-  dt <- data.table::data.table(c("A", "B", "C", "D"), c(1,2,3,4), c(0,0,0,1))
-  mat <- ggParanoid:::to_matrix(dt)
-  testthat::expect_equal(class(mat), "matrix")
-})
-
-test_that("read_homolog_matrix() produces matrix of type 'integer'", {
-  filename <- paste0(ggParanoid_example_dir(), "/homology_matrix.csv.gz")
-  hm <- ggParanoid::read_homolog_matrix(filename)
-  testthat::expect_type(hm, "integer")
-})
+#test_that("ggParanoid_example returns the correct number of files"{
+ # testthat::expect_equal(length(ggParanoid::ggParanoid_example(), 1))
+#})
