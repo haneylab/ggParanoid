c54de824b8953c87afabbbced5c3acb43b181cb8
Andrew Wilson <andrew.wilson849@icloud.com>
2019-02-08 16:03:10 -0800

    Added some tests for read_homolog_matrix

--- tests/testthat/test-read_homolog_matrix.R
+++ tests/testthat/test-read_homolog_matrix.R
@@ -1,5 +1,14 @@
-context("Example data")
+context("Data import from PyParanoid")
 
-#test_that("ggParanoid_example returns the correct number of files"{
- # testthat::expect_equal(length(ggParanoid::ggParanoid_example(), 1))
-#})
+test_that("read_matrix successfully imports a data.table object", {
+  dir = ggParanoid::ggParanoid_example_dir()
+  file = "/homology_matrix.csv.gz"
+  mat = paste0(dir, file)
+  testthat::expect_equal("data.table" %in% class(ggParanoid:::read_matrix(mat)), TRUE)
+})
+
+test_that("to_matrix() effectively converts a data.table object into a matrix", {
+  dt <- data.table::data.table(c("A", "B", "C", "D"), c(1,2,3,4), c(0,0,0,1))
+  mat <- ggParanoid:::to_matrix(dt)
+  testthat::expect_equal(class(mat) == "matrix", TRUE)
+})
